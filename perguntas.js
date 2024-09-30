bf817066584e669a9d441e57138eb458848d502d
dzubanovski <161023789+dzubanovski@users.noreply.github.com>
2024-09-30 12:21:54 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Filmes',
+    'Qual o filme que a menuna come o chiclete e fica roxa e vira uma bola?',
+    ' A fantástica fabrica de chocolate'
+)
+
+criaCartao(
+    'Filmes',
+    'Filme em que o senhor e a criança saem voando em uma casa com balões?',
+    'Up altas aventuras'
+)
+
+criaCartao(
+    'Filmes',
+    'Em qual filme a menina espeta o dedo em uma agulha de fiar?',
+    'Bela adormecida'
+)
+
+criaCartao(
+    'Filmes',
+    'Em qual filme os 7 anões aparecem?',
+    'Branca de neve e os sete anões.'
+)
